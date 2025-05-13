060dc2a856369c684402195becb000a2e3318c8d
Azmi Makarima <dev.amy.azmim@gmail.com>
2025-05-13 14:43:36 +0700

    feat: create backend query unique query logger

---
+++ apps/backend/src/db/logger.ts
@@ -0,0 +1,36 @@
+import { hash } from '@/shared/crypto'
+import type { Logger } from 'drizzle-orm/logger'
+
+const db = require('better-sqlite3')('query-log.sqlite')
+
+db.exec(`CREATE TABLE IF NOT EXISTS sql_logs (
+  id TEXT PRIMARY KEY,
+  query TEXT,
+  params TEXT,
+  query_with_params TEXT
+)`)
+
+export class UniqueLogger implements Logger {
+  logQuery(query: string, params: unknown[]): void {
+    try {
+      const id = hash(query).toString()
+      let queryWithParams = query
+      for (const param of params) {
+        if (typeof param === 'string') {
+          queryWithParams = queryWithParams.replace(`$${params.indexOf(param) + 1}`, `'${param}'`)
+        } else if (typeof param === 'number') {
+          queryWithParams = queryWithParams.replace(`$${params.indexOf(param) + 1}`, param.toString())
+        }
+      }
+
+      db.prepare('INSERT INTO sql_logs (id, query, params, query_with_params) VALUES (?, ?, ?, ?)').run(
+        id,
+        query,
+        JSON.stringify(params),
+        queryWithParams,
+      )
+    } catch (error) {
+      // Ignore errors
+    }
+  }
+}
